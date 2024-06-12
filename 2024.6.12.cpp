c58cbf151c129baa3a612ce7d12aa92403141215
2782677362 <2782677362@qq.com>
2024-06-12 22:21:10 +0800

    .

---
+++ 2024.6.12.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+//class Date {
+//public:
+//	Date(int year = 0,int month = 1,int day =1)
+//		:_year(year)
+//		,_month(month)
+//		,_day(day)
+//	{}
+//
+//	/*void f1()
+//	{}
+//	static void f2() // f2没有this指针，而调用f1时需要this指针，无法调用
+//	{
+//		f1();
+//	}*/
+//	//静态成员函数不可以调用非静态成员函数
+//
+//	static void f3()
+//	{}
+//	void f4() 
+//	{
+//		f3();
+//	}
+//	//非静态成员函数可以调用静态成员函数
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//int main()
+//{
+//	Date d1;
+//
+//	//d1.f2();
+//	d1.f4();
+//
+//	return 0;
+//}
+
+
+
+
+
+class A {
+public:
+	//void f1()//非静态函数由对象名.或者对象指针->调用
+	//{
+	//	_n++;
+	//}
+	//int RetN()
+	//{
+	//	return _n;
+	//}
+
+	static void f1()//静态成员函数则由类名::或者对象名.调用，没有this指针
+	{
+		_n++;
+	}
+	static int RetN()
+	{
+		return _n;
+	}
+private:
+	static int _n;
+};
+
+int A::_n = 0;
+
+int main()
+{
+	A::f1();
+	int ret = A::RetN();
+	cout << ret << endl;
+	return 0;
+}
