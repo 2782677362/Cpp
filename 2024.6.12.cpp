823f6c99966b83c7501075f6c3671ba5fa03c8fb
2782677362 <2782677362@qq.com>
2024-06-12 22:35:44 +0800

    .

--- 2024.6.12.cpp
+++ 2024.6.12.cpp
@@ -45,35 +45,57 @@ using namespace std;
 
 
 
-class A {
-public:
-	//void f1()//非静态函数由对象名.或者对象指针->调用
-	//{
-	//	_n++;
-	//}
-	//int RetN()
-	//{
-	//	return _n;
-	//}
+//class A {
+//public:
+//	//void f1()//非静态函数由对象名.或者对象指针->调用
+//	//{
+//	//	_n++;
+//	//}
+//	//int RetN()
+//	//{
+//	//	return _n;
+//	//}
+//
+//	static void f1()//静态成员函数则由类名::或者对象名.调用，没有this指针
+//	{
+//		_n++;
+//	}
+//	static int RetN()
+//	{
+//		return _n;
+//	}
+//private:
+//	static int _n;
+//};
+//
+//int A::_n = 0;
+//
+//int main()
+//{
+//	A::f1();
+//	int ret = A::RetN();
+//	cout << ret << endl;
+//	return 0;
+//}
 
-	static void f1()//静态成员函数则由类名::或者对象名.调用，没有this指针
-	{
-		_n++;
-	}
-	static int RetN()
-	{
-		return _n;
-	}
+
+class B
+{
+public:
+	B(int q = 0)
+		:_q(q)
+		{}
 private:
-	static int _n;
+	int _q;
 };
 
-int A::_n = 0;
+class A {
 
-int main()
-{
-	A::f1();
-	int ret = A::RetN();
-	cout << ret << endl;
-	return 0;
-}
+private:
+	//声明时给缺省值(C++11语法)
+	int _a = 1;
+	int* _b = (int*)malloc(sizeof(int) * 5);
+	B b1 = 10;
+	//B b2(10); //错误
+	//static int _n = 0; //错误，静态变量不能给缺省值
+};
